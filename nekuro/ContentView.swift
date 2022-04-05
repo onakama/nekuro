7d89bf790b586a3d13a366c64a392fecde590a20
taisei <ninzin60en.dev@gmail.com>
2022-04-05 21:16:55 +0900

    Initial Commit

---
+++ nekuro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nekuro
+//
+//  Created by 住山大誠 on 2022/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nekuro/nekuroApp.swift
@@ -0,0 +1,17 @@
+//
+//  nekuroApp.swift
+//  nekuro
+//
+//  Created by 住山大誠 on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct nekuroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
